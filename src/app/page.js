3b1f7317dce50d89f1a4a62029d5c6b2bee91707
Karan Kumar <98538984+WhiteR9se@users.noreply.github.com>
2026-05-17 15:30:58 +0530

    minor change to show maintenance

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-900">
       <h1 className="text-6xl font-bold text-green-400">
-        Hello World.
+        Coming Soon...
       </h1>
     </main>
   );
